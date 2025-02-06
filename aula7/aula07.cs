f2e21769d0907dcdbf13c58274e2da52e3317e30
Edinaldolopes <162041820+Edinaldolopes@users.noreply.github.com>
2025-02-06 18:26:49 -0300

    aula 07 constants
    trabalhando com constant

---
+++ aula7/aula07.cs
@@ -0,0 +1,13 @@
+using System;
+
+class aula07{
+
+  static void Main(){
+      const string canal="CFB cursos";
+      const double pi=3.1415;
+
+      Console.WriteLine("canal {0}", canal);
+  } 
+
+
+}
