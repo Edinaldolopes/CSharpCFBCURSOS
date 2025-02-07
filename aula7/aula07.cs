9ccf3427d0f0b15e52aebe7d65394b2915643e9d
Edinaldolopes <162041820+Edinaldolopes@users.noreply.github.com>
2025-02-06 21:32:40 -0300

    min cadastro de usuario aula 07
    min cadastro de usuario aula 07

--- aula7/aula07.cs
+++ aula7/aula07.cs
@@ -1,39 +1,38 @@
+
 using System;
 
-class aula07{
+class Aula07 {
+    static void Main() {
+        while (true) { // Loop para permitir várias execuções
+            Console.Clear(); // Limpa a tela a cada nova execução
 
-  static void Main(){
-      
-      string nome, apelido;
-      int idade;
-     
-     Console.WriteLine();
+            Console.WriteLine("=== CADASTRO DE USUÁRIO ===\n");
 
-      Console.Write("Digite seu nome.....:");
-      nome=(Console.ReadLine());
+            Console.Write("Digite seu nome.....: ");
+            // Variável
+            string nome = Console.ReadLine();
 
+            Console.Write("Digite sua idade....: ");
+            // Variável
+            int idade;
+            while (!int.TryParse(Console.ReadLine(), out idade)) {
+                Console.Write("Entrada inválida! Digite um número para idade: ");
+            }
 
-      Console.Write("digite sua idade....:");
-      idade=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite seu apelido..: ");
+            // Variável
+            string apelido = Console.ReadLine();
 
+            Console.WriteLine("\n....SEUS DADOS FORAM REGISTRADOS NO BANCO....\n");
 
-      Console.Write("Digite seu apelido..:");
-      apelido=(Console.ReadLine());
+            // Forma correta de exibir as informações
+            Console.WriteLine("NOME.....:  " + nome);
+            Console.WriteLine("IDADE....:  " + idade + " anos"); // Corrigido o erro
+            Console.WriteLine("APELIDO..:  " + apelido);
 
-      Console.WriteLine();
-
-      Console.WriteLine("....SEUS DADOS FORAM REGISTRADOS NO BANCO.... "  );
-
-      Console.WriteLine();
-
-      Console.WriteLine("NOME.....:  "  + nome  );
-      Console.WriteLine("IDADE....:  "  + idade + " anos" );
-      Console.WriteLine("APELIDO..:  "  + apelido  );
-
-       
-
-
-  } 
-
-
+            Console.WriteLine("\nDeseja cadastrar outro usuário? (S/N)");
+            string resposta = Console.ReadLine().ToUpper();
+            if (resposta != "S") break; // Sai do loop se o usuário digitar algo diferente de "S"
+        }
+    }
 }
