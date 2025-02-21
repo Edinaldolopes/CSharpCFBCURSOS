f3daa4bde07b9a90a7b5f07cd14b79bf05aa8241
Edinaldolopes <162041820+Edinaldolopes@users.noreply.github.com>
2025-02-20 22:29:25 -0300

    aula 21
    aula 21

--- aula02/obj/Debug/net8.0/aula02.AssemblyInfo.cs
+++ aula02/obj/Debug/net8.0/aula02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aula02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85e046c1daf69d687ab6f2ab3591428a1f93b881")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b2f9efffe18f0980c435d53c7d077d24ea4e268")]
 [assembly: System.Reflection.AssemblyProductAttribute("aula02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aula02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
